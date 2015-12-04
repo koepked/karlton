0b8be1d4e9df74e8a01409c6ce085ddd8f8b4be9
koepked <koepked@gmail.com>
2015-12-03 19:11:46 -0600

    Cleared up some of the SSH funny business that was occurring at init.

---
+++ src/docker/touch_nodes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import sys
+from subprocess import call
+
+HOST_FILE = '/etc/hosts'
+
+with open(HOST_FILE, 'r') as f:
+    lines = filter(lambda x:
+        'karlton' in x and len(x.split()) == 2 and 'bridge' not in x,
+        list(f)[1:])
+
+for line in lines:
+    call(['ssh', line.strip().split()[1], '"exit"'])
