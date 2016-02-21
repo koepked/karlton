3445e3517bc5c952ec514ec3fb8eb59239ae55d5
koepked <koepked@gmail.com>
2016-02-21 14:00:51 -0600

    Fixed some errors.

--- src/docker/touch_nodes.py
+++ src/docker/touch_nodes.py
@@ -7,7 +7,7 @@ HOST_FILE = '/etc/hosts'
 
 with open(HOST_FILE, 'r') as f:
     lines = filter(lambda x:
-        'karlton' in x and len(x.split()) == 2 and 'bridge' not in x,
+        'karlton' in x and len(x.split()) == 3 and 'bridge' not in x,
         list(f)[1:])
 
 for line in lines:
