7cbc3d763edc0f74e0448070f003bc574ad9b635
Dan Koepke <koepked@gmail.com>
2016-04-05 08:29:53 -0500

    Updated to be more configureable with number of nodes on each host.

--- mics/mics1/cluster/start_cluster.py
+++ mics/mics1/cluster/start_cluster.py
@@ -11,15 +11,28 @@ if __name__ == '__main__':
 	node_name_prefix = '%s-node' % image_name
 	head_name = '%s-head' % image_name
 	net_name = '%s-net' % image_name
-	num_nodes_per_host = int(sys.argv[1])
+	num_nodes_host1 = int(sys.argv[1])
+	num_nodes_host2 = int(sys.argv[2])
+	known_hosts = '/shared/root/.ssh/known_hosts'
 
 	# Setup shared files
-	with open('/shared/shared/touch_nodes.sh', 'w') as f:
-		for i in range(num_nodes_per_host * 2):
-			f.write('ssh karlton-node%d\n' % i)
+	#with open('/shared/shared/touch_nodes.sh', 'w') as f:
+		#for i in range(num_nodes_host1 + num_nodes_host2):
+			#f.write('ssh karlton-node%d\n' % i)
+
+	# Setup mpi hosts file
+	with open('/shared/shared/NAS/mpihosts', 'w') as f:
+		for i in range(num_nodes_host1 + num_nodes_host2):
+			f.write('karlton-node%d\n' % i)
+
+	# Setup mpi rank file
+	with open('/shared/shared/NAS/rankfile', 'w') as f:
+		for i in range(num_nodes_host1 + num_nodes_host2):
+			f.write('rank %d=karlton-node%d slot=0,1:0-7\n' % (i, i))
 
 	# Clear known_hosts
-	os.remove('/shared/root/.ssh/known_hosts')
+	if os.path.isfile(known_hosts):
+		os.remove(known_hosts)
 
 	# Build image
 	#ret = call(['docker', 'build', '-t', image_name, image_dir])
@@ -32,7 +45,7 @@ if __name__ == '__main__':
 		    #'--subnet=10.0.0.0/16', net_name])
 
 	# Launch compute nodes.
-	for i in range(num_nodes_per_host):
+	for i in range(num_nodes_host1):
 		node_name = '%s%d' % (node_name_prefix, i)
 		args = ['docker', 'run', '--name=%s' % node_name,
 			'--hostname=%s' % node_name, '-d',
